ea88ec01fddda0f0da6996b86a4b9bac54f50b0f
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-10-13 16:08:45 +0530

    On process JWTConfig

--- src/main/java/lk/ijse/gdse/NoteCollector/config/JWTConfig.java
+++ src/main/java/lk/ijse/gdse/NoteCollector/config/JWTConfig.java
@@ -1,4 +1,55 @@
 package lk.ijse.gdse.NoteCollector.config;
 
-public class JWTConfig {
+import ch.qos.logback.core.util.StringUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse.NoteCollector.service.JWTService;
+import lk.ijse.gdse.NoteCollector.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.thymeleaf.util.StringUtils;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class JWTConfig  extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserService userService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String initToken = request.getHeader("Authorization");
+        String userEmail;
+        String jwtToken;
+
+        //Initial validation
+        if(StringUtils.isEmpty(initToken) || !initToken.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //Token received
+        jwtToken = initToken.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
+
+        //userEmail validation
+        if(StringUtils.isNotEmpty(userEmail) &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+            //load user details based on the email
+            var loadedUser =
+                    userService.userDetailsService().loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwtToken, loadedUser)) {
+                SecurityContext emptyContext =
+                        SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken newContext =
+                        new UsernamePasswordAuthenticationToken(loadedUser, null, loadedUser.getAuthorities());
+            }
+        }
+    }
 }
