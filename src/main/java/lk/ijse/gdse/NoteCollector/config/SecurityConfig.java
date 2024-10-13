901fd7f8681ed9a14cd5ac305e5c1ec3fae25ad9
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-10-13 13:11:20 +0530

    Add SecurityConfig - creating SecurityFilterChain

--- src/main/java/lk/ijse/gdse/NoteCollector/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/NoteCollector/config/SecurityConfig.java
@@ -1,5 +1,29 @@
 package lk.ijse.gdse.NoteCollector.config;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception{
+      http.csrf(AbstractHttpConfigurer::disable)
+              .authorizeHttpRequests(req ->
+                      req.requestMatchers("api/v1/auth/").permitAll()
+                              .anyRequest()
+                              .authenticated())
+              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .authenticationProvider()
+              .addFilterBefore();
+      return http.build();
+    }
 }
