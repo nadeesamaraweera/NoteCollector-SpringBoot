ff731228122ce2a9267b191c5fc50a786b036872
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-10-13 10:23:45 +0530

    Add spring security to the project with new branch,Add InMemoryUserDetailsManager with basic auth test

---
+++ src/main/java/lk/ijse/gdse/NoteCollector/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package lk.ijse.gdse.NoteCollector.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails principleUser  = User.withDefaultPasswordEncoder()
+                .username("Nadeesha")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(principleUser);
+
+    }
+}
